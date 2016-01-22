c99abb39c5f67b98eb42a3de0725481cc6ba3306
Forest Belton <forest@homolo.gy>
2016-01-22 10:58:21 -0800

    Build jump tables from compiled ASTs

--- src/RE/CompiledAST.hs
+++ src/RE/CompiledAST.hs
@@ -3,6 +3,7 @@ module RE.CompiledAST where
 
 import Control.Monad.Free
 import Control.Monad.Free.TH
+import qualified Data.Map as M
 
 -- A list of instructions to execute, with labels
 -- taking values from `a'
@@ -15,3 +16,19 @@ data CompiledAST a next
     deriving (Functor, Show)
 
 $(makeFree ''CompiledAST)
+
+type FCompiledAST a = Free (CompiledAST a) ()
+newtype JumpTable a = JumpTable { getJumpTable :: M.Map a (FCompiledAST a) }
+    deriving (Show)
+
+buildJumpTable :: Ord a => FCompiledAST a -> JumpTable a
+buildJumpTable = JumpTable . buildJumpTable' M.empty
+
+buildJumpTable' :: Ord a => M.Map a (FCompiledAST a) -> FCompiledAST a -> M.Map a (FCompiledAST a)
+buildJumpTable' m (Pure _) = m
+buildJumpTable' m (Free x) = case x of
+    Label a next -> buildJumpTable' (M.insert a next m) next
+    Character _ next -> buildJumpTable' m next
+    Jump _ next      -> buildJumpTable' m next
+    Split _ _ next   -> buildJumpTable' m next
+    Match            -> m
